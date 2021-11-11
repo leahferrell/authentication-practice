53afe8d370234ae78549e429075e08cad01bb44f
Leah Ferrell <leahferrell03@gmail.com>
2021-11-11 12:22:42 -0600

    getting user info on profile

--- ui/src/pages/profile/Profile.js
+++ ui/src/pages/profile/Profile.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { useOktaAuth } from '@okta/okta-react'
 
 const Profile = () => {
-	const { oktaAuth } = useOktaAuth();
+	const { authState, oktaAuth } = useOktaAuth();
+	const [userInfo, setUserInfo] = useState(null);
 	const logout = async () => oktaAuth.signOut('/');
+
+	useEffect(() => {
+		if (!authState.isAuthenticated) {
+			setUserInfo(null)
+		} else {
+			oktaAuth.getUser().then(info => {
+				setUserInfo(info)
+			})
+		}
+	}, [authState, oktaAuth])
+
+	console.log(userInfo)
 
 	return (
 		<div>
 			<p>super secret profile here!!</p>
+			{userInfo && <p>Hello, {userInfo.name}!</p>}
 			<button onClick={logout}>Logout</button>
 		</div>
 	)
